e7610cdeab7342b3ace6f0788cc4627e2e3a99c0
OnPaceCoder <priyankvasoya7@gmail.com>
2022-08-05 18:11:32 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React , {useState} from "react"
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+//react-router
+import {BrowserRouter , Routes , Route , Link} from "react-router-dom"
+
+
+//toast
+import {ToastContainer} from "react-toastify"
+import "react-toastify/dist/ReactToastify.min.css"
+
+
+//firebase
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+
+
+//components 
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import PageNotFound from "./pages/PageNotFound"
+import Footer  from "./layout/Footer";
+import Header from "./layout/Header";
+
+
+//context
+import { UserContext  } from "./context/UserContext";
+
+
+
+import firebaseConfig from "./Config/FirebaseConfig"
+
+
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+
+function App() {
+
+const [user, setUser] = useState(null)
+
+  return (
+
+    <BrowserRouter>
+  <ToastContainer/>
+    <UserContext.Provider value={{user,setUser}}>
+      <Header/>
+      <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Signin" element={<Signin/>}/>
+    <Route path="/Signup" element={<Signup/>}/>
+    <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+      <Footer/>
+    </UserContext.Provider>
+
+
+    </BrowserRouter>
+    
+
+  );
+}
+
+export default App;
---
+++ src/layout/Header.js
@@ -0,0 +1,93 @@
+import React , {useState , useContext} from "react"
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav , NavItem , NavLink , NavbarText
+} from "reactstrap"
+
+import {Link} from "react-router-dom"
+
+import { UserContext } from "../context/UserContext"
+
+
+
+const Header = () => {
+    const context = useContext(UserContext)
+    const [isOpen , setIsOpen] = useState(false)
+
+    const toggle = () => setIsOpen(!isOpen)
+
+    return (
+        <Navbar color="info " light  expand="md">
+
+            <NavbarBrand>
+            <Link to="/" className="text-white text-decoration-none">
+            PG Vasoya GitFire app
+            </Link>
+            </NavbarBrand>
+
+            <NavbarText className="text-white">
+            {
+                context.user?.email ? context.user.email: ""
+            }
+            </NavbarText>
+
+
+            <NavbarToggler onClick={toggle}/>
+
+
+
+
+            <Collapse isOpen={isOpen} navbar>
+            <Nav className="ms-auto" navbar >
+                {
+                    context.user ? ( 
+                    
+                    <NavItem>
+                        <NavLink
+                            tag={Link}
+                            to="/"
+        
+                        className="text-white text-decoration-none">
+                            LogOut 
+                        </NavLink>
+                    </NavItem> ) : ( 
+                    <>
+                    <NavItem>
+            <NavLink
+                    tag={Link}
+                    to="/"
+                    
+                    className="text-white text-decoration-none">
+                    SignIn 
+                </NavLink>
+            </NavItem>
+            <NavItem>
+            <NavLink
+                    tag={Link}
+                    to="/"
+                    
+                    className="text-white text-decoration-none">
+                    SignUp 
+                </NavLink>
+            </NavItem>
+                    </>
+           )
+                }
+           
+
+
+            </Nav>
+            </Collapse>
+
+
+
+        </Navbar>
+    )
+}
+
+
+export default Header;
